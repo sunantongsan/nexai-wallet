b377d3b45de9419e5b99d4d4f3bacc0295f94c42
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2025-11-16 14:06:41 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
 // Utility functions for Wallet
 console.log("Utils loaded");
 
-// คุณสามารถเพิ่มฟังก์ชันเช่น checksum, validate address, hash, seed phrase เป็นต้น
+// (คุณสามารถเพิ่ม checksum หรือ hash ได้ในอนาคต)
